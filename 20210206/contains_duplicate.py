a3deacc332a63677cb5d08b9b18a18687898c157
jyeoniii <jaykim9438@gmail.com>
2021-02-06 09:54:11 +0900

    20210206: Contains Duplicate

---
+++ 20210206/contains_duplicate.py
@@ -0,0 +1,12 @@
+# https://leetcode.com/problems/contains-duplicate/solution/
+
+from typing import List
+
+
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        s = set()
+        for num in nums:
+            if num in s: return True
+            s.add(num)
+        return False
